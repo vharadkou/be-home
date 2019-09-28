a76d55895e6cdfd55021f31fe03eec4a82833de6
Artsiom Marozau <artsiom_marozau@epam.com>
2019-09-28 15:46:39 +0300

    fix: styles for guid

--- src/components/GuidCard.tsx
+++ src/components/GuidCard.tsx
@@ -4,12 +4,12 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import IconButton from "@material-ui/core/IconButton";
-import BookmarkIcon from "@material-ui/icons/Bookmark";
-import ChatIcon from "images/svg/Chat.svg";
-import Fab from "@material-ui/core/Fab";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,9 +20,72 @@ const useStyles = makeStyles((theme: Theme) =>
     media: {
       height: 0,
       paddingTop: "56.25%" // 16:9
+    },
+    text: {
+      fontStyle: "normal",
+      fontWeight: "normal",
+      fontSize: "16px",
+      lineHeight: "24px"
+    },
+    smallText: {
+      fontSize: "14px"
     }
   })
 );
+
+const ITEM_HEIGHT = 48;
+
+const LongMenu = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const classes = useStyles();
+
+  return (
+    <>
+      <IconButton
+        aria-label="more"
+        aria-controls="long-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: 200
+          }
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <span className={clsx(classes.text, classes.smallText)}>
+            Отправить сообщение
+          </span>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <span className={clsx(classes.text, classes.smallText)}>
+            Добавить в закладки
+          </span>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
 
 export default function RecipeReviewCard({
   fullName,
@@ -36,23 +99,18 @@ export default function RecipeReviewCard({
 
   return (
     <Card className={classes.card}>
-      <div style={{ position: "absolute", zIndex: 2, top: "165px", right: 0 }}>
-        <CardActions>
-          <Fab size="small" style={{ backgroundColor: "#FFFFFF" }}>
-            <IconButton aria-label="add to favorites">
-              <BookmarkIcon />
-            </IconButton>
-          </Fab>
-          <Fab size="small" style={{ backgroundColor: "#FFFFFF" }}>
-            <IconButton aria-label="share">
-              <img alt="Chat icon" src={ChatIcon}></img>
-            </IconButton>
-          </Fab>
-        </CardActions>
-      </div>
       <CardActionArea>
         <CardMedia className={classes.media} image={imgSrc} title="" />
-        <CardHeader title={`${fullName}, ${price}$`} subheader={description} />
+        <CardHeader
+          action={<LongMenu />}
+          title={<span className={classes.text}>{fullName}</span>}
+          subheader={
+            <span className={clsx(classes.text, classes.smallText)}>
+              «{description}»
+            </span>
+          }
+          disableTypography={false}
+        />
       </CardActionArea>
       <CardContent>
         {/* <Typography variant="body2" color="textSecondary" component="p">
