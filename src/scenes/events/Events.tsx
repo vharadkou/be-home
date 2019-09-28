ad2db02b1c4624270e31e8102d6c0759046b4ddb
Valiantsin <valiantsin.haradkou@icloud.com>
2019-09-28 17:24:20 +0300

    feat: events

--- src/scenes/events/Events.tsx
+++ src/scenes/events/Events.tsx
@@ -1,5 +1,79 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
+import { useStore } from 'stores';
+import Box from '@material-ui/core/Box';
+import { events } from './events-data';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from '@material-ui/core/styles';
 
-export const Events = () => {
-  return <div>Events</div>;
-};
+const useStyles = makeStyles(theme => ({
+  root: {},
+  card: {
+    marginBottom: '12px',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  text: {
+    fontSize: '1rem',
+  },
+  textSmall: {
+    fontSize: '0.875rem',
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+  cardHeader: {
+    paddingTop: theme.spacing(1),
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+  date: {
+    textTransform: 'lowercase',
+  },
+}));
+
+export const Events = observer(() => {
+  const classes = useStyles();
+  const { uiStore } = useStore();
+
+  useEffect(() => {
+    uiStore.loadGuides();
+  }, [uiStore]);
+
+  const formatedDate = (date: Date) => {
+    const digestDate: number = date.getDate();
+    const digestMonth: string = date
+      .toLocaleString('ru', { month: 'short' })
+      .toUpperCase();
+    const digestYear: number = date.getFullYear();
+
+    return `${digestDate} ${digestMonth} ${digestYear}`;
+  };
+
+  return (
+    <Box>
+      {events.map((event, index) => (
+        <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia className={classes.media} image={event.path} title="" />
+          </CardActionArea>
+          <CardHeader
+            className={classes.cardHeader}
+            title={<span className={classes.text}>{event.title}</span>}
+            subheader={
+              <>
+                <div>
+                  <span className={classes.textSmall}>{event.description}</span>
+                </div>
+                <div className={classes.date}>{formatedDate(event.date)}</div>
+              </>
+            }
+            disableTypography={false}
+          />
+        </Card>
+      ))}
+    </Box>
+  );
+});
---
+++ src/scenes/events/events-data.ts
@@ -0,0 +1,27 @@
+import Event1 from 'images/event1.png';
+import Event2 from 'images/event2.png';
+import Event3 from 'images/event3.jpg';
+
+export const events = [
+  {
+    path: Event1,
+    title: 'Mogilev Hackathon “Smart City”',
+    description:
+      'Приглашаем всех заинтересованных принять участие с друзьями и коллегами',
+    date: new Date(2019, 9, 28),
+  },
+  {
+    path: Event3,
+    title: 'JS Machine Meet Up #1',
+    description:
+      'Тайм-кафе НЕБО, Lieninskaja Street, Mogilev, Belarus. Script it!',
+    date: new Date(2019, 10, 5),
+  },
+  {
+    path: Event2,
+    title: 'Mogilev stand-up',
+    description:
+      'Приглашаем всех заинтересованных принять участие с друзьями и коллегами',
+    date: new Date(2019, 10, 26),
+  },
+];
